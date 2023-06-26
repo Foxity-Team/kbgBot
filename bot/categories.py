25a4709d1f2abec544724194ec7609a72771fc84
Neso Hiroshi <122040999+nesohoshi@users.noreply.github.com>
2023-06-26 23:43:35 +0500

    Update categories.py

--- bot/categories.py
+++ bot/categories.py
@@ -49,7 +49,7 @@ def buildHelpEmbed() -> Embed:
         catName, _ = categ.getData()
         emb.add_field(name=f'{i+1}. {catName}', value='', inline=False)
 
-    emb.add_field(name="Что бы узнать команді из категории, напишите:", value="`kgb!help (цифра категории)`", inline=False)
+    emb.add_field(name="Что бы узнать команди из категории, напишите:", value="`kgb!help (цифра категории)`", inline=False)
     emb.set_thumbnail(url="https://media.discordapp.net/attachments/1068579157493153863/1094662619211780096/Bez_nazvania2_20230409092059.png")
     emb.set_footer(text="communist_fox", icon_url="https://media.discordapp.net/attachments/1068579157493153863/1094468823542943765/R44rlXiYjWw.jpg?width=425&height=425")
     
