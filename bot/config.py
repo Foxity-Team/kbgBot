b28dbd7c741d3d43d72576fb5946d08bd6081914
Neso Hiroshi <122040999+nesohoshi@users.noreply.github.com>
2023-06-28 09:47:18 +0500

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,3 +1,3 @@
 prefix = ["kgb!", "$sudo ", "please, dear bot, take me a", "aid!"]
 on_guild_join_pic = "https://media.discordapp.net/attachments/1068579157493153863/1094662619211780096/Bez_nazvania2_20230409092059.png"
-botURL = f"https://discord.com/api/oauth2/authorize?client_id=1061907927880974406}&permissions=8&scope=bot"
+botURL = "https://discord.com/api/oauth2/authorize?client_id=1061907927880974406}&permissions=8&scope=bot"
