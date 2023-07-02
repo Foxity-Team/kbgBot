75ed62da7bad47141a137d572815571cf7f8d963
Neso Hiroshi <122040999+nesohoshi@users.noreply.github.com>
2023-07-02 14:37:17 +0500

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -32,7 +32,7 @@ from dotenv import load_dotenv
 from categories import buildHelpEmbed, buildCategoryEmbeds, helpCategory
 from balaboba import Balaboba
 from config import *
-import agify
+from agify import AsyncNameAPI
 
 print(g4f.Provider.Ails.params)
 
