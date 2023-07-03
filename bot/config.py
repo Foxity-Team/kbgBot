45d3501da6e07259a52076be663ec9f1a6aa808a
Neso Hiroshi <122040999+nesohoshi@users.noreply.github.com>
2023-07-03 18:20:37 +0500

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -6,7 +6,7 @@ secretURL = 'https://media.discordapp.net/attachments/1067069690066767924/109538
 nasaKEY = "oEUDnRapyzulvTNbWimSBmFldgwMZt5ZZgU547Xf"
 chatbotKEY = 'wlkMplI6cPas78JtMwzKpwgO5EqNUw7fsXtpm2bmLE332cuHN3VZXIs17QdQ0pi1'
 cipherURL = 'https://media.discordapp.net/attachments/977992655466270730/1073628659417632828/qr-code.png?width=425&height=425'
-avaURL = "https://media.discordapp.net/attachments/1068579157493153863/1094468823542943765/R44rlXiYjWw.jpg?width=425&height=425"
+avaURL = "https://media.discordapp.net/attachments/1056839870535979070/1076440402367885403/0a24dfcff79ee1f4.png?width=403&height=403"
 boostyURL = "https://boosty.to/kgbmodernbot"
 tumbaYUMBA = "https://media.discordapp.net/attachments/1068579157493153863/1094662619211780096/Bez_nazvania2_20230409092059.png"
 ver = "0.1.0 \n0.2.0 \n0.3.0 \n0.4.0 \n0.5.0 \n0.6.0 \n0.6.3 \n0.6.4 \n1.0 \n1.1 \n1.2 \n1.2.5 \n1.3 \n1.5 \n1.5.5 \n1.6 \n1.7 \n1.8 \n2.0 \n2.5(нынешняя)"
