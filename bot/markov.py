0d520babb4cd25234750dfb175731fc38bd5cbce
Griss <grisshink@mail.ru>
2023-07-05 16:10:09 +0300

    Fix empty messages

--- bot/markov.py
+++ bot/markov.py
@@ -52,7 +52,7 @@ class MarkovGen:
                 print(outString.capitalize())
                 sub = re.sub('(<@&*[0-9]*>)', '', outString.capitalize())
                 print(sub)
-                if sub == '':
+                if sub.strip() == '':
                     return '*(Пустое сообщение)*'
                 return sub 
             return outString.capitalize()
