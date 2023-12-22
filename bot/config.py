6cd6ac3740c946ef0acb44efd9c8cef1c38de835
Neso Hiroshi <122040999+nesohoshi@users.noreply.github.com>
2023-12-22 14:29:21 +0500

    Поменял
    Изменил инфу

--- bot/config.py
+++ bot/config.py
@@ -1,4 +1,4 @@
-prefix = ["kgb!", "$sudo ", "please, dear bot, take me a", "aid!"]
+prefix = ["kgb!", "$sudo ", "please, dear bot, take me a", "!~", "~execute"]
 on_guild_join_pic = "https://media.discordapp.net/attachments/1068579157493153863/1094662619211780096/Bez_nazvania2_20230409092059.png"
 botURL = "https://discord.com/api/oauth2/authorize?client_id=1061907927880974406&permissions=8&scope=bot"
 serverURL = "https://discord.gg/CDMaFC84JE"
@@ -9,4 +9,4 @@ cipherURL = 'https://media.discordapp.net/attachments/977992655466270730/1073628
 avaURL = "https://media.discordapp.net/attachments/1056839870535979070/1076440402367885403/0a24dfcff79ee1f4.png?width=403&height=403"
 boostyURL = "https://boosty.to/kgbmodernbot"
 tumbaYUMBA = "https://media.discordapp.net/attachments/1068579157493153863/1094662619211780096/Bez_nazvania2_20230409092059.png"
-ver = "0.1.0 \n0.2.0 \n0.3.0 \n0.4.0 \n0.5.0 \n0.6.0 \n0.6.3 \n0.6.4 \n1.0 \n1.1 \n1.2 \n1.2.5 \n1.3 \n1.5 \n1.5.5 \n1.6 \n1.7 \n1.8 \n2.0 \n2.5(нынешняя)"
+ver = "0.1.0 \n0.2.0 \n0.3.0 \n0.4.0 \n0.5.0 \n0.6.0 \n0.6.3 \n0.6.4 \n1.0 \n1.1 \n1.2 \n1.2.5 \n1.3 \n1.5 \n1.5.5 \n1.6 \n1.7 \n1.8 \n2.0 \n2.5 \n3.0(нынешняя)"
