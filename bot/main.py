a4e9b5641cd920de4efabc30818403a7c2f37028
Neso Hiroshi <122040999+nesohoshi@users.noreply.github.com>
2023-12-22 14:17:57 +0500

    Изменил
    Изменил версию бота на новую в статусе

--- bot/main.py
+++ bot/main.py
@@ -64,7 +64,7 @@ if not os.path.isfile('data/guild_seek.json'):
         f.write('{}')
 
 async def change_status():
-    statuses = "kgb!help", "версия 2.5", "на {} серверах!", "SLAVA KPSS!"
+    statuses = "kgb!help", "версия 3.0", "на {} серверах!", "SLAVA KPSS!"
     index = 0
     while not kgb.is_closed():
         servers_count = len(kgb.guilds)
